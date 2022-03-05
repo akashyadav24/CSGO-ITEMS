44498f8577cec3688425557a55dbffb0932b8e75
Mihail Dragnev <bymykel5@gmail.com>
2022-03-05 18:07:33 +0100

    feat: add 404 page.

---
+++ pages/404.js
@@ -0,0 +1,17 @@
+export default function Custom404() {
+  return (
+    <div className="absolute inset-0 flex flex-col justify-center h-screen px-4 md:px-0 md:items-center md:flex-row">
+      <div>
+        <h1 className="text-3xl font-bold text-indigo-500 md:text-5xl">404</h1>
+      </div>
+      <div className="md:pl-4 md:ml-4 md:border-l border-stone-200">
+        <h1 className="mb-1 text-3xl font-bold md:text-5xl text-stone-900">
+          Page Not Found
+        </h1>
+        <p className="text-stone-500">
+          Please check the URL in the address bar and try again.
+        </p>
+      </div>
+    </div>
+  );
+}
