5bd60f12424d8753544eae8770e10b24d05eead9
Mihail Dragnev <bymykel5@gmail.com>
2022-03-05 19:19:31 +0100

    feat: add collectibles.

--- services/csgo.js
+++ services/csgo.js
@@ -40,3 +40,59 @@ export const getStickers = async () => {
 export const getCollections = async () => {
   return await axios.get(`${BASE_URL}/collections.json`);
 };
+
+export const getCollectibles = async (type = undefined) => {
+  if (type !== "major" && type !== "operation") {
+    const file =
+      type === undefined ? "/collectibles.json" : `/collectibles/${type}.json`;
+    return await axios.get(`${BASE_URL}${file}`);
+  }
+
+  if (type === "major") {
+    const major = { data: [] };
+
+    await axios
+      .get(`${BASE_URL}/collectibles/major/fantasy_trophies.json`)
+      .then((response) => {
+        major.data.push(...response.data);
+      });
+
+    await axios
+      .get(`${BASE_URL}/collectibles/major/finalists_trophies.json`)
+      .then((response) => {
+        major.data.push(...response.data);
+      });
+
+    await axios
+      .get(`${BASE_URL}/collectibles/major/pickem_coins.json`)
+      .then((response) => {
+        major.data.push(...response.data);
+      });
+
+    await axios
+      .get(`${BASE_URL}/collectibles/major/pickem_old.json`)
+      .then((response) => {
+        major.data.push(...response.data);
+      });
+
+    return major;
+  }
+
+  if (type === "operation") {
+    const operation = { data: [] };
+
+    await axios
+      .get(`${BASE_URL}/collectibles/operation/coins.json`)
+      .then((response) => {
+        operation.data.push(...response.data);
+      });
+
+    await axios
+      .get(`${BASE_URL}/collectibles/operation/stars.json`)
+      .then((response) => {
+        operation.data.push(...response.data);
+      });
+
+    return operation;
+  }
+};
