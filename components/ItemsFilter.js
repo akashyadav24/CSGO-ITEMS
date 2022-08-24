cf738678142a0063c4c9791e45af496fb9092799
Mihail Dragnev <bymykel5@gmail.com>
2022-08-24 18:34:28 +0200

    feat: add serch bar for crates.

--- components/ItemsFilter.js
+++ components/ItemsFilter.js
@@ -1,13 +1,22 @@
 import Link from "next/link";
+import { useRouter } from "next/router";
 import { useEffect, useState, useRef } from "react";
 
-export default function ItemsFilter({ search, setSearch }) {
+function classNames(...classes) {
+  return classes.filter(Boolean).join(" ");
+}
+
+export default function ItemsFilter({ filter, filterOptions, search, setSearch }) {
   const [dropdown, setDropdown] = useState(false);
   const wrapperRef = useRef("menu");
+  const router = useRouter();
 
   useEffect(() => {
     function handleClickOutside(event) {
-      if (wrapperRef.current && !wrapperRef?.current?.contains(event.target)) {
+      if (
+        wrapperRef.current &&
+        !wrapperRef?.current?.contains?.(event.target)
+      ) {
         setDropdown(false);
       }
     }
@@ -18,7 +27,7 @@ export default function ItemsFilter({ search, setSearch }) {
   }, [wrapperRef]);
 
   return (
-    <div className="mt-10 -mb-5">
+    <div className="flex justify-end mt-10 -mb-5">
       <div className="relative flex max-w-md rounded-md">
         <div className="absolute inset-y-0 flex items-center left-2 text-slate-400">
           <svg
@@ -51,51 +60,65 @@ export default function ItemsFilter({ search, setSearch }) {
             input && input.focus();
           }}
           type="text"
-          className="w-full p-2 pl-10 border rounded-md outline-none bg-stone-50 border-stone-200"
+          className={classNames(
+            filter ? "border-r-0 rounded-r-none" : "",
+            "w-full p-2 pl-10 text-sm border rounded-md outline-none bg-stone-50 border-stone-200"
+          )}
           placeholder="Search..."
           value={search}
           onChange={(e) => setSearch(e.target.value)}
         ></input>
-        {/* <div
-          ref={wrapperRef}
-          className="relative flex items-center px-4 border border-l rounded-md rounded-l-none bg-stone-100 border-stone-200"
-        >
-          <button
-            className="flex items-center"
-            onClick={() => setDropdown(!dropdown)}
+        {filter && (
+          <div
+            ref={wrapperRef}
+            className="relative flex items-center px-4 border border-l rounded-md rounded-l-none bg-stone-100 border-stone-200"
           >
-            <span className="mr-1 text-sm text-gray-700">Category</span>
-            <svg
-              className="w-5 h-5 text-gray-400"
-              fill="currentColor"
-              viewBox="0 0 20 20"
-              xmlns="http://www.w3.org/2000/svg"
+            <button
+              className="flex items-center"
+              onClick={() => setDropdown(!dropdown)}
             >
-              <path
-                fillRule="evenodd"
-                d="M5.293 7.293a1 1 0 011.414 0L10 10.586l3.293-3.293a1 1 0 111.414 1.414l-4 4a1 1 0 01-1.414 0l-4-4a1 1 0 010-1.414z"
-                clipRule="evenodd"
-              ></path>
-            </svg>
-          </button>
-          {dropdown && (
-            <div className="absolute right-0 z-50 w-40 py-2 overflow-hidden rounded-md bg-stone-100 top-11">
-              <ul className="relative">
-                <li className="hover:bg-stone-50">
-                  <Link href="">
-                    <a
-                      className="flex items-center w-full p-2"
-                      onClick={() => setSearch("")}
+              <span className="mr-1 text-sm text-gray-700 w-max">{ filterOptions.find(item => item.href === router.asPath)?.name ?? 'Category'}</span>
+              <svg
+                className="w-5 h-5 text-gray-400"
+                fill="currentColor"
+                viewBox="0 0 20 20"
+                xmlns="http://www.w3.org/2000/svg"
+              >
+                <path
+                  fillRule="evenodd"
+                  d="M5.293 7.293a1 1 0 011.414 0L10 10.586l3.293-3.293a1 1 0 111.414 1.414l-4 4a1 1 0 01-1.414 0l-4-4a1 1 0 010-1.414z"
+                  clipRule="evenodd"
+                ></path>
+              </svg>
+            </button>
+            {dropdown && (
+              <div className="absolute right-0 z-40 w-40 py-2 overflow-hidden border rounded-md shadow-lg bg-stone-50 ring-1 ring-black ring-opacity-5 top-11">
+                <ul className="relative text-sm">
+                  {filterOptions.map((item, index) => (
+                    <li
+                      key={index}
+                      className="text-gray-700 hover:bg-gray-100 hover:text-gray-900"
                     >
-                      {" "}
-                      All{" "}
-                    </a>
-                  </Link>
-                </li>
-              </ul>
-            </div>
-          )}
-        </div> */}
+                      <Link href={item.href}>
+                        <a
+                          className={classNames(
+                            router.asPath === item.href
+                              ? "bg-gray-100 text-gray-900"
+                              : "",
+                            "flex items-center w-full p-1.5"
+                          )}
+                          onClick={() => setSearch("")}
+                        >
+                          {item.name}
+                        </a>
+                      </Link>
+                    </li>
+                  ))}
+                </ul>
+              </div>
+            )}
+          </div>
+        )}
       </div>
     </div>
   );
