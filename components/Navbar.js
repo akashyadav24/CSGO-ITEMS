efcbc9238f4d7427e1ad6c72ef48fef228051680
Mihail Dragnev <bymykel5@gmail.com>
2022-10-30 00:09:41 +0200

    feat: add github link.

--- components/Navbar.js
+++ components/Navbar.js
@@ -50,10 +50,10 @@ export default function Navbar({ openPalette, togglePalette }) {
                 </Link>
               </div>
 
-              <div className="relative ml-auto pointer-events-auto">
+              <div className="relative flex ml-auto space-x-3 pointer-events-auto">
                 <button
                   type="button"
-                  className="hidden w-full lg:flex items-center text-sm leading-6 text-slate-400 rounded-md ring-1 ring-slate-900/10 shadow-sm py-1.5 pl-2 pr-5 hover:ring-slate-300"
+                  className="items-center hidden lg:flex text-slate-400 hover:text-slate-500"
                   onClick={() => togglePalette(true)}
                 >
                   <svg
@@ -61,7 +61,7 @@ export default function Navbar({ openPalette, togglePalette }) {
                     height="24"
                     fill="none"
                     aria-hidden="true"
-                    className="flex-none mr-3"
+                    className="flex-none"
                   >
                     <path
                       d="m19 19-3.5-3.5"
@@ -80,8 +80,22 @@ export default function Navbar({ openPalette, togglePalette }) {
                       strokeLinejoin="round"
                     ></circle>
                   </svg>
-                  Search
                 </button>
+                <Link href="https://github.com/ByMykel/CSGO-ITEMS">
+                  <a
+                    className="items-center hidden lg:flex text-slate-400 hover:text-slate-500"
+                    target="_blank"
+                  >
+                    <svg
+                      viewBox="0 0 16 16"
+                      className="w-5 h-5"
+                      fill="currentColor"
+                      aria-hidden="true"
+                    >
+                      <path d="M8 0C3.58 0 0 3.58 0 8c0 3.54 2.29 6.53 5.47 7.59.4.07.55-.17.55-.38 0-.19-.01-.82-.01-1.49-2.01.37-2.53-.49-2.69-.94-.09-.23-.48-.94-.82-1.13-.28-.15-.68-.52-.01-.53.63-.01 1.08.58 1.23.82.72 1.21 1.87.87 2.33.66.07-.52.28-.87.51-1.07-1.78-.2-3.64-.89-3.64-3.95 0-.87.31-1.59.82-2.15-.08-.2-.36-1.02.08-2.12 0 0 .67-.21 2.2.82.64-.18 1.32-.27 2-.27.68 0 1.36.09 2 .27 1.53-1.04 2.2-.82 2.2-.82.44 1.1.16 1.92.08 2.12.51.56.82 1.27.82 2.15 0 3.07-1.87 3.75-3.65 3.95.29.25.54.73.54 1.48 0 1.07-.01 1.93-.01 2.2 0 .21.15.46.55.38A8.013 8.013 0 0016 8c0-4.42-3.58-8-8-8z"></path>
+                    </svg>
+                  </a>
+                </Link>
               </div>
 
               <div className="relative items-center hidden ml-auto lg:flex">
@@ -113,7 +127,7 @@ export default function Navbar({ openPalette, togglePalette }) {
               </div>
               <button
                 type="button"
-                className="flex items-center justify-center w-8 h-8 ml-auto -my-1 text-slate-500 hover:text-slate-600 lg:hidden"
+                className="flex items-center justify-center w-8 h-8 ml-auto -my-1 text-slate-400 hover:text-slate-500 lg:hidden"
                 onClick={() => togglePalette(true)}
               >
                 <svg
@@ -130,7 +144,22 @@ export default function Navbar({ openPalette, togglePalette }) {
                   <circle cx="11" cy="11" r="6"></circle>
                 </svg>
               </button>
-              <div className="ml-2 -my-1 lg:hidden">
+              <Link href="https://github.com/ByMykel/CSGO-ITEMS">
+                <a
+                  className="flex items-center ml-2 lg:hidden text-slate-400 hover:text-slate-500"
+                  target="_blank"
+                >
+                  <svg
+                    viewBox="0 0 16 16"
+                    className="w-5 h-5"
+                    fill="currentColor"
+                    aria-hidden="true"
+                  >
+                    <path d="M8 0C3.58 0 0 3.58 0 8c0 3.54 2.29 6.53 5.47 7.59.4.07.55-.17.55-.38 0-.19-.01-.82-.01-1.49-2.01.37-2.53-.49-2.69-.94-.09-.23-.48-.94-.82-1.13-.28-.15-.68-.52-.01-.53.63-.01 1.08.58 1.23.82.72 1.21 1.87.87 2.33.66.07-.52.28-.87.51-1.07-1.78-.2-3.64-.89-3.64-3.95 0-.87.31-1.59.82-2.15-.08-.2-.36-1.02.08-2.12 0 0 .67-.21 2.2.82.64-.18 1.32-.27 2-.27.68 0 1.36.09 2 .27 1.53-1.04 2.2-.82 2.2-.82.44 1.1.16 1.92.08 2.12.51.56.82 1.27.82 2.15 0 3.07-1.87 3.75-3.65 3.95.29.25.54.73.54 1.48 0 1.07-.01 1.93-.01 2.2 0 .21.15.46.55.38A8.013 8.013 0 0016 8c0-4.42-3.58-8-8-8z"></path>
+                  </svg>
+                </a>
+              </Link>
+              <div className="ml-4 -my-1 lg:hidden">
                 <button
                   type="button"
                   className="flex items-center justify-center w-8 h-8 text-slate-500 hover:text-slate-600"
