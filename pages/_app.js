18ee44aee8ec83dffe032b5c488e0599f9d61b73
Mihail Dragnev <bymykel5@gmail.com>
2022-11-04 17:24:15 +0100

    refactor: replace Google Analytics tracking code.

--- pages/_app.js
+++ pages/_app.js
@@ -2,7 +2,6 @@ import CommandPalette from "../components/CommandPalette";
 import Navbar from "../components/Navbar";
 import { useState, useEffect } from "react";
 import "../styles/globals.css";
-import Script from "next/script";
 
 function MyApp({ Component, pageProps }) {
   const [open, setOpen] = useState(false);
@@ -19,22 +18,6 @@ function MyApp({ Component, pageProps }) {
 
   return (
     <>
-      <Script
-        strategy="lazyOnload"
-        src={`https://www.googletagmanager.com/gtag/js?id=G-NYJQBFE09Z`}
-      />
-
-      <Script id="google-analytics" strategy="lazyOnload">
-        {`
-          window.dataLayer = window.dataLayer || [];
-          function gtag(){dataLayer.push(arguments);}
-          gtag('js', new Date());
-          gtag('config', 'G-NYJQBFE09Z', {
-          page_path: window.location.pathname,
-          });
-        `}
-      </Script>
-
       <Navbar openPalette={open} togglePalette={setOpen} />
       <CommandPalette openPalette={open} togglePalette={setOpen} />
       <main className="px-4 mx-auto lg:px-8 max-w-7xl">
