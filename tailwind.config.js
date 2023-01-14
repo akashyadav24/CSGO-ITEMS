f4588ce460bd44ed39a95d067b6c6dbe9731538c
Mihail Dragnev <bymykel5@gmail.com>
2023-01-14 14:55:03 +0100

    feat: redesign navbar component.

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,4 +1,9 @@
+const svgToDataUri = require('mini-svg-data-uri');
 const defaultTheme = require("tailwindcss/defaultTheme");
+
+const {
+  default: flattenColorPalette,
+} = require('tailwindcss/lib/util/flattenColorPalette');
 
 module.exports = {
   mode: "jit",
@@ -17,5 +22,18 @@ module.exports = {
     },
   },
   darkMode: 'class',
-  plugins: [],
+  plugins: [
+    function ({ matchUtilities, theme }) {
+      matchUtilities(
+        {
+          'bg-grid': (value) => ({
+            backgroundImage: `url("${svgToDataUri(
+              `<svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 32 32" width="32" height="32" fill="none" stroke="${value}" stroke-dasharray="5 3" transform="scale(1, -1)"><path d="M0 .5H31.5V32"/></svg>`
+            )}")`,
+          }),
+        },
+        { values: flattenColorPalette(theme('backgroundColor')), type: 'color' }
+      );
+    },
+  ],
 };
