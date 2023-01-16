618fbae51f3bd6551fe7c8b2c89c817f5ce20638
Mihail Dragnev <bymykel5@gmail.com>
2023-01-16 18:05:15 +0100

    refactor: remove Google analytics.

--- pages/_document.js
+++ pages/_document.js
@@ -6,23 +6,6 @@ function Document() {
       <Head>
         <link rel="stylesheet" href="https://rsms.me/inter/inter.css" />
         <link rel="icon" href="/favicon.jpg" />
-
-        <script
-          async
-          src="https://www.googletagmanager.com/gtag/js?id=G-NYJQBFE09Z"
-        />
-        <script
-          dangerouslySetInnerHTML={{
-            __html: `
-            window.dataLayer = window.dataLayer || [];
-            function gtag(){dataLayer.push(arguments);}
-            gtag('js', new Date());
-            gtag('config', 'G-NYJQBFE09Z', {
-              page_path: window.location.pathname,
-            });
-          `,
-          }}
-        />
       </Head>
       <body>
         <Main />
